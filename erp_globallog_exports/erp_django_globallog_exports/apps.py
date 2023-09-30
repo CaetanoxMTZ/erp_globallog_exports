1e3add45506b645b940c57a7ab5a4b141de4165c
Matheus Caetano <114543730+CaetanoxMTZ@users.noreply.github.com>
2023-09-29 23:55:27 -0300

    Commit inicial projeto Django

---
+++ erp_globallog_exports/erp_django_globallog_exports/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ErpDjangoGloballogExportsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'erp_django_globallog_exports'
